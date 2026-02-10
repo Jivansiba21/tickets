7b85ce153d49ac59ac6d3e153d18abf195fd55d7
jivansiba <jivansibaasodha@gmail.com>
2026-02-10 15:07:11 +0530

    Github test

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,9 @@ Route::get('/page', function () {
     return view('page');
 });
 
-
+Route::get('/github-test', function () {
+    return view('welcome');
+});
 Route::get('/login',[AuthController::class,'showLogin'])->name('login');
 Route::get('/logout',[AuthController::class,'logout'])->name('logout');
 Route::post('/authenticate', [AuthController::class, 'Authentication'])->name('login.post');
